9ab339a68238312b5f366ec125abaa64f645e133
seongbeop <seongbeop0@gmail.com>
2024-01-29 16:15:47 +0900

    240129 java-basic static-ex

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -15,5 +15,14 @@ public class DecoDataMain {
         System.out.println("3. 인스턴스 호출2");
         DecoData data2 = new DecoData();
         data2.instanceCall();
+
+        //추가
+        //인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        //클래스를 통한 접근
+        DecoData.staticCall();
+
     }
 }
